fd2595d89f1e115fb1c604ae4355f7d4ca6ae268
Kyle Hensman <kyle@eastcode.ca>
2022-03-07 18:39:04 -0400

    FIX: updated owner in test to trigger a fail

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -66,8 +66,8 @@ class OwnerControllerTests {
 	private Owner george() {
 		Owner george = new Owner();
 		george.setId(TEST_OWNER_ID);
-		george.setFirstName("George");
-		george.setLastName("Franklin");
+		george.setFirstName("Dave");
+		george.setLastName("Wattson");
 		george.setAddress("110 W. Liberty St.");
 		george.setCity("Madison");
 		george.setTelephone("6085551023");
